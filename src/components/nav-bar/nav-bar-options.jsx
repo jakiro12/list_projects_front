4e683fc986aad8205ced828af86bf163b6746eab
Jakiro12 <eclipsisnocte@gmail.com>
2024-07-25 11:03:06 -0300

    feat:new shadows

--- src/components/nav-bar/nav-bar-options.jsx
+++ src/components/nav-bar/nav-bar-options.jsx
@@ -12,12 +12,16 @@ export default function NavBarForOptions({setActionType, typeAction}){
                     <button
                         onClick={()=>setActionType('add')}
                     >+ Add Project</button>
-                </aside>: <button
-                        className='nav-bar_btn-back'
-                        onClick={()=>setActionType(null)}
-                >&#10094;Back
-                <span style={{fontWeight:'bold'}}>{typeAction === 'add' ? 'Add Project' : 'Edit Project'}</span>
-                </button>}
+                </aside>:
+                <aside className='nav-bar_section-add'>
+                    <button
+                            className='nav-bar_btn-back'
+                            onClick={()=>setActionType(null)}
+                    >&#10094;  Back
+                    </button>
+                    <span style={{fontWeight:'bold'}}>{typeAction === 'add' ? 'Add Project' : 'Edit Project'}</span>
+                </aside>
+                }
                 
             </div>
         </nav>
